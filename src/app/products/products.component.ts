8a7d963eed4d18613bd1b753c0f2e3ce6480025c
unknown <domgjonikrist@gmail.com>
2020-02-15 19:10:45 +0100

    displaying the categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-products',
@@ -8,8 +9,12 @@ import { ProductService } from '../product.service';
 })
 export class ProductsComponent{
   products$;
-  constructor(private productService: ProductService) { 
+  categories$;
+
+  constructor(private productService: ProductService, private categoryService: CategoryService) { 
     this.products$ = this.productService.getAll();
+
+    this.categories$ = this.categoryService.getCategories();
   }
 
 }
