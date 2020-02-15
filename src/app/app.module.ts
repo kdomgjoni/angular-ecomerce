a954f615d0a9acc984b83f538a3eacefa92206b5
unknown <domgjonikrist@gmail.com>
2020-02-15 14:03:18 +0100

    adding a data table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { environment } from './../environments/environment.prod';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DataTableModule } from 'angular-4-data-table';
 
 import { AppComponent } from './app.component';
 
@@ -50,6 +51,7 @@ import { CustomFormsModule } from 'ng2-validation';
     CustomFormsModule,
     BrowserModule,
     FormsModule,
+ 
     NgbModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // firestore
