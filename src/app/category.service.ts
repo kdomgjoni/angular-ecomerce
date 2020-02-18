bdbb2bd482dfbd0e1d4c79b62d314b39fa9bb270
unknown <domgjonikrist@gmail.com>
2020-02-18 23:45:24 +0100

    filtering products by category

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { Product } from './models/products';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +11,14 @@ export class CategoryService {
 
   constructor(private db: AngularFireDatabase) { 
   }
-  getCategories() {
-    return this.db.list('/categories').valueChanges();
+  // getCategories() {
+  //   return this.db.list('/categories').valueChanges();
     
+  // }
+  getCategories() : Observable<Product[]>{  return this.db.list('/categories').snapshotChanges().pipe(
+    map(changes => 
+      changes.map(c => ({ key: c.payload.key, ...c.payload.val() as Product }))
+    )
+  );
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/products';
 
 @Component({
   selector: 'app-products',
@@ -8,13 +10,26 @@ import { CategoryService } from '../category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent{
-  products$;
+  products: Product[] = [];
+  filteredProduct: Product[] = [];
   categories$;
+  category: string;
 
-  constructor(private productService: ProductService, private categoryService: CategoryService) { 
-    this.products$ = this.productService.getAll();
+  constructor(private productService: ProductService, 
+    private categoryService: CategoryService,
+    private route: ActivatedRoute) 
+    { 
+      this.productService.getAll().subscribe(products => this.products = products);
+      this.categories$ = this.categoryService.getCategories();
 
-    this.categories$ = this.categoryService.getCategories();
+    //We can't use snapshot here becaus the url changes in the DOM so we have to subscribe
+      this.route.queryParamMap.subscribe(param => {
+        this.category = param.get('category');
+        this.filteredProduct = (this.category) ?
+          this.products.filter(p => p.category === this.category) : 
+          this.products;
+        console.log(this.filteredProduct)
+      })
   }
 
 }
