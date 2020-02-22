99ef6be6c7f8727d19dc0117ccf92925958a6cef
unknown <domgjonikrist@gmail.com>
2020-02-22 16:26:12 +0100

    Adding a Product to the Shopping Cart

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -16,6 +16,6 @@ export class ProductCardComponent {
 
 
   addToCart(product: Product){
-    
+    this.cartService.addToCart(product);
   }
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { create } from 'domain';
+import { Product } from './models/products';
+import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +13,43 @@ export class ShoppingCartService {
   constructor(private db: AngularFireDatabase) { }
 
 
-  create(){
+  private create(){
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     })
   }
 
+  
   private getCart(cartId){
     this.db.object('/shopping-cart' + cartId);
   }
 
-  private async getOrCreateCart(){
+  //"async - awit" is the same like "then" promises
+  private async getOrCreateCartId(){
     let cartId = localStorage.getItem('cartId');
-
-    if(!cartId){
+    if(cartId) return cartId
+    
       let result = await this.create();
       localStorage.setItem('cartId', result.key);
-      return this.getCart(cartId);
-    }
-    return this.getCart(cartId);
+      return result.key;
+    
+  }
+
+  // Because the "getOrCreateCartId()" method is observable and return a promise we can use async or "then"
+  async addToCart(product: Product){
+    const cartId = await this.getOrCreateCartId();
+    const item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.key);
+
+    item$.valueChanges().pipe(take(1))
+    .subscribe(item => {
+      if (item) {
+        item$.update({quantity: item['quantity'] + 1});
+      } else {
+        item$.set({ product: product, 
+          quantity: 1 });
+      }
+    });
+
   }
 }
 
